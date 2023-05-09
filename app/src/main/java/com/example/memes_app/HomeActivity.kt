de7a0eef46c238cc6501a2de2facc06b3665b3d5
Vishma Pratim Das <bhishmadas9@gmail.com>
2023-05-09 07:35:06 +0530

    v1.0 (stable) of meme App

---
+++ app/src/main/java/com/example/memes_app/HomeActivity.kt
@@ -0,0 +1,39 @@
+package com.example.memes_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        loadMeme()
+    }
+
+    fun loadMeme() {
+        val url:String = "https://meme-api.com/gimme"
+        val imgMeme=findViewById<ImageView>(R.id.imgMeme)
+        val btnNext=findViewById<Button>(R.id.btnNext)
+        val btnShare=findViewById<Button>(R.id.btnShare)
+
+        btnNext.setOnClickListener {
+            this@HomeActivity.loadMeme()
+        }
+        val queue=JsonObjectRequest (Request.Method.GET, url, null, Response.Listener { response ->
+            val url=response.getString("url")
+            Glide.with(this).load(url).into(imgMeme)
+        }, Response.ErrorListener {
+            Toast.makeText(this@HomeActivity, "Something went wrong, please try again.", Toast.LENGTH_LONG)
+        })
+    }
+}
